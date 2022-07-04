da88efafd87a5662029b6e08aee539f2034d625a
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-07-04 13:27:50 -0600

    fix: stylelint bug

--- src/components/listItem/ListItem.js
+++ src/components/listItem/ListItem.js
@@ -3,53 +3,53 @@ import './Listitem.css';
 import Input from './input';
 
 const data = {
-    todos: [
-        {
-            id: 1,
-            title: 'Setup development environment',
-            completed: true,
-        },
-        {
-            id: 2,
-            title: 'Develop website and add content',
-            completed: false,
-        },
-        {
-            id: 2,
-            title: 'Develop website and add content',
-            completed: false,
-        },
-        {
-            id: 3,
-            title: 'Deploy to live server',
-            completed: false,
-        },
-        {
-            id: 3,
-            title: 'Deploy to live server',
-            completed: false,
-        },
-        {
-            id: 3,
-            title: 'Deploy to live server',
-            completed: false,
-        },
-    ],
+  todos: [
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ],
 };
 
 function ListItem() {
-    return (
-        <div className="list">
-            <ul className="listItem">
-                <h1> ListItemTask</h1>
-                <Input />
-                {data.todos.map((item) => (
-                    <li key={item.id}>{item.title}</li>
+  return (
+    <div className="list">
+      <ul className="listItem">
+        <h1> ListItemTask</h1>
+        <Input />
+        {data.todos.map((item) => (
+          <li key={item.id}>{item.title}</li>
 
-                ))}
-            </ul>
-        </div>
-    );
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ListItem;
