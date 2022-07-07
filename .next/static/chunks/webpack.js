cb037f01cdfbe9e612e184fe57fca59bef098a2f
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-07-07 05:49:59 -0600

    BREAKING-CHANGES: added support for tailwind css

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "dfc57c2ba330da94"; }
+/******/ 		__webpack_require__.h = function() { return "a946c21eacc44206"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
